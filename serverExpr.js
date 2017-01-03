4c247520de80e67f8b8f54dfd7a7b06c2f488b27
mientje <mientjepeutie@outlook.com>
2017-01-03 19:27:06 +0000

    delete process.env.IP from app.listen

--- serverExpr.js
+++ serverExpr.js
@@ -18,6 +18,6 @@ app.get('/:datumType', function(request, response) {
 	response.send(datum);       
 });
 
-app.listen(process.env.PORT || 8000, process.env.IP, function(){
+app.listen(process.env.PORT || 8000, function(){
     console.log("er gebeurt wat");
 });
